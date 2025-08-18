5be34a41cfff815f86eeebd4007ac9d59211e80c
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-08-18 16:09:57 +0300

    cleanup

--- src/client/java/top/offsetmonkey538/tinyterrors/client/render/ModEntityModelLayers.java
+++ src/client/java/top/offsetmonkey538/tinyterrors/client/render/ModEntityModelLayers.java
@@ -2,7 +2,6 @@ package top.offsetmonkey538.tinyterrors.client.render;
 
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.minecraft.client.model.Dilation;
-import net.minecraft.client.render.entity.ZombieEntityRenderer;
 import net.minecraft.client.render.entity.model.*;
 
 import java.util.Set;
@@ -11,17 +10,14 @@ import static top.offsetmonkey538.tinyterrors.TinyTerrors.id;
 
 public final class ModEntityModelLayers {
 
-    //public static final EntityModelLayer CREEPER_BABY = register("creeper_baby", () -> CreeperEntityModel.getTexturedModelData(Dilation.NONE).transform(new BabyModelTransformer(Set.of("head"))));
-    //public static final EntityModelLayer CREEPER_BABY = register("creeper_baby", () -> CreeperEntityModel.getTexturedModelData(Dilation.NONE).transform(ZombieEntityModel.BABY_TRANSFORMER));
-    public static final EntityModelLayer CREEPER_BABY = register("creeper_baby", () -> CreeperEntityModel.getTexturedModelData(Dilation.NONE).transform(modelData -> {
-        //return ZombieEntityModel.BABY_TRANSFORMER.apply(modelData);
-        /// boolean scaleHead, float babyYHeadOffset, float babyZHeadOffset, float babyHeadScale, float babyBodyScale, float bodyYOffset, Set<String> headParts
-        return new BabyModelTransformer(true, 14.0F, 0.0F, 2.0F, 2.0F, 24.0F, Set.of("head")).apply(modelData);
-    }));
+    public static final EntityModelLayer CREEPER_BABY = register("creeper_baby", () -> CreeperEntityModel.getTexturedModelData(Dilation.NONE), 14);
 
-    private static EntityModelLayer register(final String id, final EntityModelLayerRegistry.TexturedModelDataProvider provider) {
+    private static EntityModelLayer register(final String id, final EntityModelLayerRegistry.TexturedModelDataProvider baseProvider, final float headOffset) {
         final EntityModelLayer layer = new EntityModelLayer(id(id), "main");
-        EntityModelLayerRegistry.registerModelLayer(layer, provider);
+        final BabyModelTransformer modelTransformer = new BabyModelTransformer(true, headOffset, 0.0F, 2.0F, 2.0F, 24.0F, Set.of("head"));
+
+        EntityModelLayerRegistry.registerModelLayer(layer, () -> baseProvider.createModelData().transform(modelTransformer));
+
         return layer;
     }
 
