304866940989bec9899f733caf929d45fd748210
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-08-21 10:35:46 +0300

    Make felix happy

---
+++ src/main/java/top/offsetmonkey538/tinyterrors/MandatoryEmptyClass.java
@@ -0,0 +1,7 @@
+package top.offsetmonkey538.tinyterrors;
+
+public final class MandatoryEmptyClass {
+    private MandatoryEmptyClass() {
+        System.out.println("Are you happy now, mr felix?");
+    }
+}
